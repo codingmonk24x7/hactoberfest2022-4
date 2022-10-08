1d5037daacd2402db59e62816583ca41ab8bd864
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-09 00:45:03 +0530

    PalindromeNumber

---
+++ PalindromeNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str1 = Integer.toString(x);
+        String str2 = new StringBuilder().append(x).reverse().toString();
+        
+
+        if(str1.equals(str2))
+            return true;
+        
+        return false;
+        
+        //for one lines of code just return below statement
+         // return Integer.toString(x).equals(new StringBuilder().append(x).reverse().toString()); 
+        
+    }
+}
